45ec7e2acf9fe1d9e7191c9329d61b8715d2007d
laird-ikar <bguyot@k1r1p10.42mulhouse.fr>
2022-02-25 09:48:27 +0100

    2h30 bitch

---
+++ get_next_line_utils.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bguyot <bguyot@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 09:25:09 by bguyot            #+#    #+#             */
+/*   Updated: 2022/02/25 08:53:38 by bguyot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	const char	*s;
+
+	s = str;
+	while (*s)
+		s++;
+	return (s - str);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+	int		step;
+
+	if (dst == NULL && src == NULL)
+		return (NULL);
+	if (len > 0 && dst != src)
+	{
+		i = 0;
+		step = 1;
+		if (src < dst)
+		{
+			i = len - 1;
+			step = -1;
+		}
+		while (i >= 0 && i < len)
+		{
+			((char *) dst)[i] = ((char *) src)[i];
+			i += step;
+		}
+	}
+	return (dst);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*res;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	res = malloc((s1_len + s2_len + 1) * sizeof (char));
+	if (!res)
+		return (NULL);
+	ft_memmove((void *) res, (void *) s1, s1_len);
+	ft_memmove((void *) res + s1_len, (void *) s2, s2_len + 1);
+	return (res);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(char *)s = 0;
+		s++;
+		i++;
+	}
+}
